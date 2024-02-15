0973fbb1ecf21cc69bb970f11486ff2bc9ed4d03
Samuel Finatto <samuelfinatto@hotmail.com>
2024-02-15 19:30:51 -0300

    update code

--- BlazorOficinar.AppHost/Program.cs
+++ BlazorOficinar.AppHost/Program.cs
@@ -1,4 +1,3 @@
-using BlazorOficinar.ServiceDefaults.Models;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -6,9 +5,5 @@ var cache = builder.AddRedisContainer("cache");
 
 var postgresdb = builder.AddPostgresContainer("pg")
                         .AddDatabase("postgresdb");
-
-builder.AddProject<Projects.BlazorOficinar>("oficinar")
-    .WithReference(cache)
-    .WithReference(postgresdb);
 
 builder.Build().Run();
--- BlazorOficinar/Program.cs
+++ BlazorOficinar/Program.cs
@@ -1,15 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorOficinar;
-using BlazorOficinar.ServiceDefaults.Models;
-using Aspire.Npgsql;
-using Aspire.Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.AddNpgsqlDbContext<OficinarContext>("db");
+//builder.AddNpgsqlDbContext<OficinarContext>("db");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
