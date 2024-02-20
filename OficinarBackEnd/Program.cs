363f4df74ac43d68d142866962598f8c1b9d6c52
Samuel Finatto <samuelfinatto@hotmail.com>
2024-02-20 20:39:34 -0300

    add signal R

--- OficinarBackEnd/Program.cs
+++ OficinarBackEnd/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using OficinarBackEnd.Hubs;
 using OficinarBackEnd.Models;
 using OficinarBackEnd.Repositories;
 
@@ -18,9 +20,20 @@ builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 builder.Services.AddDbContext<OficinarContext>(
         options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
+
+app.UseResponseCompression();
+app.MapHub<ChatHub>("/chathub");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
